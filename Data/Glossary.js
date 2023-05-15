c3922287b82983b5aa9f55295912789c3e4916c6
makwojwwa <118631284+makwojwwa@users.noreply.github.com>
2023-05-15 18:14:26 +0200

    Add files via upload

---
+++ Data/Glossary.js
@@ -0,0 +1,1 @@
+define({numchunks:1,prefix:'Glossary_Chunk',terms:[{t:'Assignment',c:0},{t:'Bonder',c:0},{t:'Fusion Laser Target',c:0},{t:'Grid cell',c:0},{t:'Instruction',c:0},{t:'Object',c:0},{t:'Reactor',c:0},{t:'Sensor',c:0},{t:'Structure',c:0},{t:'Waldo',c:0}]});
---
+++ Data/Search.js
@@ -0,0 +1,1 @@
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/Game mechanics/Advanced instructions.htm'],s:['0'],p:[['0',19],['fusion',4],['recycler',3]]});
